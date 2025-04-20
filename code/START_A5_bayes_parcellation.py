bbcc975467c9f557790ae02e9a16f2904a30b677
jkderrick028 <jkderrick028@gmail.com>
2025-04-19 20:28:20 -0400

    lut

--- code/START_A5_bayes_parcellation.py
+++ code/START_A5_bayes_parcellation.py
@@ -91,7 +91,7 @@ def plot_probseg(nifti,cmap):
 
 # Load colormap and labels
 # lid,cmap,names = nt.read_lut('atl-NettekovenSym32.lut')
-lid,cmap,names = nt.read_lut('atl-glasser.lut')
+lid,cmap,names = nt.read_lut(os.path.join(surface_helpers_dir, 'atl-glasser.lut'))
 
 # Make a nifti image of the first subject
 nifti = atlas.data_to_nifti(U)
