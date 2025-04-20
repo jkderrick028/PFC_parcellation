1d2beaf1cda965fc71abfa737e5c284a29ea92ca
jkderrick028 <jkderrick028@gmail.com>
2025-04-20 15:47:19 -0400

    saving U

--- code/START_A5_bayes_parcellation.py
+++ code/START_A5_bayes_parcellation.py
@@ -109,7 +109,7 @@ def plot_probseg(surf_data, cmap):
     # left cortex
     flatmap.plot(label_L.reshape(-1, ),
                  surf=flat_surf_L,
-                 underlay='FLAT.surf.gii',
+                 underlay=flat_surf_L,
                  alpha=1,
                  label_names=names,
                  new_figure=False,
@@ -124,7 +124,7 @@ def plot_probseg(surf_data, cmap):
     # right cortex
     flatmap.plot(label_R.reshape(-1, ),
                  surf=flat_surf_R,
-                 underlay='FLAT.surf.gii',
+                 underlay=flat_surf_R,
                  alpha=1,
                  label_names=names,
                  new_figure=False,
