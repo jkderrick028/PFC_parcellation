37a4288ae4cde3d99654fd035695a7c1833d775a
jkderrick028 <jkderrick028@gmail.com>
2025-11-27 14:30:46 -0500

    curve fitting

--- code/evaluations.py
+++ code/evaluations.py
@@ -171,7 +171,14 @@ def spatial_ACF_per_voxel_cv(maxDist=35, binWidth=1, func=None, dist=None):
             var_ = var[p, inBin]
             this_corr = np.nanmean(cov_[~np.isnan(var_)]) / np.nanmean(var[p, inBin])
             corrs[p, i+1] = this_corr
-        params, params_cov = curve_fit(laplacian_pdf, dists, corrs[p], nan_policy='omit')
+
+        # try:
+        #     params, params_cov = curve_fit(laplacian_pdf, dists, corrs[p], nan_policy='omit', maxfev=10000)
+        # except:
+        #     breakpoint()
+
+        params, params_cov = curve_fit(laplacian_pdf, dists, corrs[p], nan_policy='omit', maxfev=10000)
+
         FWHMs.append(params[0])
 
     FWHMs = np.array(FWHMs)
