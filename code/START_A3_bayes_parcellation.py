dc2c729d82aac8ffd18f30147045bf06f78497a3
jkderrick028 <jkderrick028@gmail.com>
2025-12-09 18:47:28 -0500

    update

--- code/START_A3_bayes_parcellation.py
+++ code/START_A3_bayes_parcellation.py
@@ -161,7 +161,8 @@ def run_bayes_parcellation(ROI):
     group_parcellation = U.copy()
     # group_parcellation[excluded_vtx_inds_LR] = 181
     group_parcellation[excluded_vtx_inds_LR] = np.max(group_parcellation) + 1
-    group_parcellation[inds_U_zero] = 0
+    # group_parcellation[inds_U_zero] = 0
+    group_parcellation[inds_U_zero] = np.max(group_parcellation) + 1
 
     ## U_individual: the data likelihood, n_subjects x n_parcels x n_vertices in whole cortxe
     U_individual_restore = np.zeros((n_subjects, K, n_vertices_whole_cortex))
