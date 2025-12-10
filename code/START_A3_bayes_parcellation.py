dbbc40b24ef16b1e9d186c7de90a0666728ffbd0
jkderrick028 <jkderrick028@gmail.com>
2025-12-10 11:14:36 -0500

    check label 0

--- code/START_A3_bayes_parcellation.py
+++ code/START_A3_bayes_parcellation.py
@@ -26,8 +26,8 @@ def run_bayes_parcellation(ROI):
     dataset_name = 'MDTB' # or Demand
 
     # atlas_name = 'yeo17'
-    # atlas_name = 'schaefer100'
-    atlas_name = 'glasser'
+    atlas_name = 'schaefer100'
+    # atlas_name = 'glasser'
 
     strength = 20.0
 
@@ -237,3 +237,5 @@ if __name__=='__main__':
 
     for roi in ROIs:
         run_bayes_parcellation(roi)
+
+    # run_bayes_parcellation('PFC')
